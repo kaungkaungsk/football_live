ff05bad0685649bcab07716af7f3677067eee70e
cyber <kyawgyi16.knp@gmail.com>
2024-02-25 10:53:10 +0700

    update

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy Policy</title>
+    <style>
+        .wrapper {
+            padding: 0.5rem 2rem;
+        }
+
+        .bold {
+            font-weight: 800;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <h2>Privacy Policy for The Reds Show</h2>
+
+        <span class="bold">Effective Date:</span> February 25, 2024
+
+        <h3>Introduction</h3>
+
+        <p>This Privacy Policy explains how we at [Your Name/Company Name] collect, use, and disclose information from
+            users
+            of
+            our mobile application, "The Reds Show" (the "App"). We are committed to protecting your privacy and
+            providing a
+            safe and secure experience for all users.</p>
+
+        <h3>Information We Collect</h3>
+
+        <p>Device Information: We collect basic device information, such as device type, operating system version, and
+            language
+            settings, to ensure compatibility and optimize app performance.
+            Usage Data: We collect anonymized usage data, such as app launch times, screen interactions, and features
+            used,
+            to
+            improve the App and understand user behavior. This data does not personally identify you.
+            How We Use the Information</p>
+
+        <h4>We use the information we collect to:</h4>
+
+        <p><span class="bold">Operate and improve the App:</span> We use device information and anonymized usage data
+            to
+            diagnose and fix issues,
+            develop
+            new features, and customize the app experience.</p>
+        <p>
+            <span class="bold">Send push notifications:</span> We use Firebase to send push notifications with updates,
+            news, and promotional
+            offers
+            related to "The Reds Show." You can opt out of receiving notifications in your device settings.
+            Analytics: We use anonymized usage data for analytics purposes to understand user trends and preferences.
+            This
+            data
+            does not personally identify you.
+        </p>
+
+        <h3>Information Sharing</h3>
+
+        <p>We do not share any personally identifiable information (PII) with third parties for advertising or marketing
+            purposes. However, we may share anonymized or aggregated data with third-party service providers who help us
+            operate
+            the App, such as analytics providers. These service providers are bound by contractual obligations to
+            protect
+            your
+            data confidentiality and security.</p>
+
+        <h3>Firebase</h3>
+
+        <p>We use Firebase for push notifications and app diagnostics. Firebase collects and processes limited device
+            information, such as device tokens and anonymous usage data. This information is used for the purposes
+            mentioned
+            above and subject to Google's Privacy Policy: https://policies.google.com/privacy</p>
+
+        <h3>Children's Privacy</h3>
+
+        <p>We are committed to protecting the privacy of children. Our App targets users aged 13 and above. We do not
+            knowingly
+            collect or solicit personal information from children under 13. If you are a parent or guardian and believe
+            your
+            child has provided us with personal information, please contact us at lonelycoder101@gmail.com. We will take
+            steps
+            to delete that information.</p>
+
+        <h3>Your Choices</h3>
+
+        <p><span class="bold">Push Notifications:</span> You can opt out of receiving push notifications in your
+            device settings.
+        </p>
+
+        <p>
+            <span class="bold"> Data Deletion:</span> You can request deletion of your anonymized usage data by
+            contacting us at
+            lonelycoder101@gmail.com.
+            Please note that this may affect your app experience.
+            Security
+        </p>
+
+        <span class="bold">We take reasonable measures to protect user information from unauthorized access,
+            disclosure, alteration, or
+            destruction. However, no internet or electronic data transmission is completely secure.</span>
+
+        <h3>Changes to This Policy</h3>
+
+        <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new
+            Privacy
+            Policy on this page.</p>
+
+        <h3>Contact Us</h3>
+
+        <p>If you have any questions about this Privacy Policy, please contact us at:
+
+            lonelycoder101@gmail.com</p>
+    </div>
+
+</body>
+
+</html>
